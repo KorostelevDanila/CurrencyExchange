ee4fbbee216fc1155059521710780310b9ebcf10
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-01-29 16:32:06 +0300

    add DBManager to Repository class

--- src/main/java/repositories/Repository.java
+++ src/main/java/repositories/Repository.java
@@ -1,8 +1,11 @@
 package repositories;
 
+import dbManager.DBManager;
+
 import java.util.List;
 
 public abstract class Repository<T> {
+    DBManager dbManager;
     public abstract List<T> findAll();
     public abstract T findById(Long id);
 }
