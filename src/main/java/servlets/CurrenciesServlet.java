e854ce7c72fe37ae27b0ea9d32ce9293c06f77fb
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-01-29 17:00:33 +0300

    test repository and dbmanager

--- src/main/java/servlets/CurrenciesServlet.java
+++ src/main/java/servlets/CurrenciesServlet.java
@@ -1,17 +1,38 @@
 package servlets;
 
 import java.io.*;
+import java.util.Currency;
+import java.util.List;
 
+import dbManager.SQLiteDBManager;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import models.CurrencyModel;
+import repositories.CurrenciesRepository;
 
 @WebServlet(name = "CurrenciesServlet", value = "/currencies")
 public class CurrenciesServlet extends HttpServlet {
+    CurrenciesRepository currenciesRepository;
+
+    public CurrenciesServlet() throws ClassNotFoundException {
+        currenciesRepository = new CurrenciesRepository(new SQLiteDBManager());
+    }
 
     // Endpoint: GET /currencies
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
+        List<CurrencyModel> currencies = currenciesRepository.findAll();
+
+        response.setContentType("text/html");
+
+        PrintWriter pw = response.getWriter();
+
+        for (var a : currencies) {
+            pw.println(a.toString() + "\n");
+        }
+
+        //response.setContentType("application/json");
+
     }
 
     // Endpoint: POST /currencies
