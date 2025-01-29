8608cf332636f22b4c37a7155465d3708f77035b
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-01-29 16:47:35 +0300

    create CurrenciesRepository

---
+++ src/main/java/repositories/CurrenciesRepository.java
@@ -0,0 +1,49 @@
+package repositories;
+
+import dbManager.DBManager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.CurrencyModel;
+
+public class CurrenciesRepository extends Repository<CurrencyModel> {
+
+    public CurrenciesRepository(DBManager dbManager) {
+        this.dbManager = dbManager;
+    }
+
+    @Override
+    public List<CurrencyModel> findAll() {
+        String query = "SELECT * FROM Currencies";
+
+        List<CurrencyModel> currencies = new ArrayList<>();
+
+        Connection conn = dbManager.getConnection();
+        try (Statement statement = conn.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                String code = resultSet.getString("Code");
+                String fullName = resultSet.getString("FullName");
+                String sign = resultSet.getString("Sign");
+                CurrencyModel currencyModel = new CurrencyModel(id, code, fullName, sign);
+                currencies.add(currencyModel);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return currencies;
+    }
+
+    @Override
+    public CurrencyModel findById(Long id) {
+        return null;
+    }
+}
