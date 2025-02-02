59fcc44cbfe9a845972e2c4da59e2a78e390ee92
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-02 14:59:33 +0300

    add SQLException throws in Repository

--- src/main/java/repositories/Repository.java
+++ src/main/java/repositories/Repository.java
@@ -2,10 +2,11 @@ package repositories;
 
 import dbManager.DBManager;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public abstract class Repository<T> {
     DBManager dbManager;
-    public abstract List<T> findAll();
-    public abstract T findById(Long id);
+    public abstract List<T> findAll() throws SQLException;
+    public abstract T findById(Long id) throws SQLException;
 }
