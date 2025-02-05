bc6a0439e79817edc5137773f607e2398c52b08f
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-05 09:02:41 +0300

    add insert method to Repository abstract class

--- src/main/java/repositories/Repository.java
+++ src/main/java/repositories/Repository.java
@@ -9,4 +9,5 @@ public abstract class Repository<T> {
     DBManager dbManager;
     public abstract List<T> findAll() throws SQLException;
     public abstract T findById(Long id) throws SQLException;
+    public abstract T insert(T object) throws SQLException;
 }
