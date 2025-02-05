d090ee7010701ec6d46fa0f171a25f1ae0e02735
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-05 12:21:01 +0300

    create getExchangeRateModel(ResultSet) method

--- src/main/java/repositories/ExchangeRateRepository.java
+++ src/main/java/repositories/ExchangeRateRepository.java
@@ -4,7 +4,12 @@ import dbManager.DBManager;
 import models.CurrencyModel;
 import models.ExchangeRateModel;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExchangeRateRepository extends Repository<ExchangeRateModel> {
@@ -28,5 +33,15 @@ public class ExchangeRateRepository extends Repository<ExchangeRateModel> {
     @Override
     public ExchangeRateModel insert(ExchangeRateModel object) throws SQLException {
         return null;
+    }
+
+    private ExchangeRateModel getExchangeRateModel(ResultSet resultSet) throws SQLException {
+        int ID = resultSet.getInt("ID");
+        long baseCurrencyId = resultSet.getInt("BaseCurrencyId");
+        long targetCurrencyId = resultSet.getInt("TargetCurrencyId");
+        BigDecimal rate = resultSet.getBigDecimal("Rate");
+        CurrencyModel baseCurrency = currenciesRepository.findById(baseCurrencyId);
+        CurrencyModel targetCurrency = currenciesRepository.findById(targetCurrencyId);
+        return new ExchangeRateModel(ID, baseCurrency, targetCurrency, rate);
     }
 }
