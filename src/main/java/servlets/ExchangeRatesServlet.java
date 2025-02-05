3059964648fb7b85235ac66a22650e23a831e84a
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-05 11:24:32 +0300

    implement basic logic for GET method

--- src/main/java/servlets/ExchangeRatesServlet.java
+++ src/main/java/servlets/ExchangeRatesServlet.java
@@ -1,15 +1,36 @@
 package servlets;
 
 import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
 
+import dbManager.SQLiteDBManager;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import models.ExchangeRateModel;
+import org.json.JSONArray;
+import repositories.ExchangeRateRepository;
 
 @WebServlet(name = "ExchangeRatesServlet", value = "/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
+    ExchangeRateRepository exchangeRateRepository;
+
+    public ExchangeRatesServlet() throws ClassNotFoundException {
+        exchangeRateRepository = new ExchangeRateRepository(new SQLiteDBManager());
+    }
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
+        PrintWriter pw = response.getWriter();
+
+        try {
+            List<ExchangeRateModel> exchangeRates = exchangeRateRepository.findAll();
+            JSONArray jsonArray = new JSONArray(exchangeRates);
+            pw.write(jsonArray.toString());
+        } catch (SQLException e) {
+            throw new RuntimeException(e); //TODO implement SQLException handling
+        }
     }
 
     @Override
