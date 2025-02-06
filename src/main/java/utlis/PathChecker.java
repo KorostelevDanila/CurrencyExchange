c97c10411706b6f4bda7cd91b4a3a0d9e64866bd
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-06 13:26:59 +0300

    create exchange rate path check method

--- src/main/java/utlis/PathChecker.java
+++ src/main/java/utlis/PathChecker.java
@@ -1,9 +1,28 @@
 package utlis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PathChecker {
     public static String checkCurrencyPath(String path) {
         if (path != null && !path.equals("/")) {
             return path.replaceFirst("/", "");
+        }
+
+        return null;
+    }
+
+    public static Map<String, String> checkExchangeRatePath(String path) {
+        if (path != null && !path.equals("/")) {
+            path = path.replaceFirst("/", "");
+            if (path.length() == 6) {
+                String from = path.substring(0, 3);
+                String to = path.substring(3);
+                Map<String, String> exchangePair = new HashMap<>();
+                exchangePair.put("from", from);
+                exchangePair.put("to", to);
+                return exchangePair;
+            }
         }
 
         return null;
