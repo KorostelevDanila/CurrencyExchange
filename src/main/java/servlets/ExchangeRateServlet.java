ab2fca49c0a32a137270f9bae64567f0f07ca12f
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-06 14:17:20 +0300

    add repositories to ExchangeRateServlet

--- src/main/java/servlets/ExchangeRateServlet.java
+++ src/main/java/servlets/ExchangeRateServlet.java
@@ -2,12 +2,22 @@ package servlets;
 
 import java.io.*;
 
+import dbManager.SQLiteDBManager;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import repositories.CurrenciesRepository;
+import repositories.ExchangeRateRepository;
 
 @WebServlet(name = "ExchangeRateServlet", value = "/exchangeRate")
 public class ExchangeRateServlet extends HttpServlet {
+    ExchangeRateRepository exchangeRateRepository;
+    CurrenciesRepository currenciesRepository;
+
+    public ExchangeRateServlet() throws ClassNotFoundException {
+        this.currenciesRepository = new CurrenciesRepository(new SQLiteDBManager());
+        this.exchangeRateRepository = new ExchangeRateRepository(new SQLiteDBManager(), currenciesRepository);
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
