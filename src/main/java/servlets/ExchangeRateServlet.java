cec9f33696bceb88ccfc4aeee2456f889f590c44
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-06 14:18:05 +0300

    implement GET method

--- src/main/java/servlets/ExchangeRateServlet.java
+++ src/main/java/servlets/ExchangeRateServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.*;
+import java.sql.SQLException;
+import java.util.Map;
 
 import dbManager.SQLiteDBManager;
 import exceptions.NotFoundInDatabaseException;
@@ -26,6 +28,29 @@ public class ExchangeRateServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
+        PrintWriter pw = response.getWriter();
+
+        String path = request.getPathInfo();
+
+        Map<String, String> exchangePair = PathChecker.checkExchangeRatePath(path);
+
+        if (exchangePair == null) {
+            String message = "Неправильный запрос обеменного курса. Нужно предоставить пару кодов валют, например /USDRUB";
+            JSONObject jsonObject = new JSONObject().put("message", message);
+            pw.write(jsonObject.toString());
+            response.setStatus(400);
+        } else {
+            try {
+                ExchangeRateModel exchangeRateModel = exchangeRateRepository.findByExchangePair(exchangePair);
+                JSONObject jsonObject = new JSONObject(exchangeRateModel);
+                pw.write(jsonObject.toString());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (NotFoundInDatabaseException e) {
+                String message = e.getMessage();
+                JSONResponser.sendJSONErrorMessage(message, 404, response);
+            }
+        }
     }
 
     @Override
