ef5b10a1624f75ba854670a21533d880ffc447e5
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-06 14:15:40 +0300

    add constructors to NotFoundInDatabaseException

--- src/main/java/exceptions/NotFoundInDatabaseException.java
+++ src/main/java/exceptions/NotFoundInDatabaseException.java
@@ -1,4 +1,11 @@
 package exceptions;
 
 public class NotFoundInDatabaseException extends RuntimeException {
+    public NotFoundInDatabaseException() {
+        super();
+    }
+
+    public NotFoundInDatabaseException(String message) {
+        super(message);
+    }
 }
