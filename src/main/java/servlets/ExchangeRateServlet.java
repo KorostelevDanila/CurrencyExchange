e7fab3df0d6410e2fd3b53f5b443528a1d12859c
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-11 19:32:06 +0300

    implement PATCH method for ExchangeRateServlet

--- src/main/java/servlets/ExchangeRateServlet.java
+++ src/main/java/servlets/ExchangeRateServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.*;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.Map;
 
@@ -13,6 +14,7 @@ import models.ExchangeRateModel;
 import org.json.JSONObject;
 import repositories.CurrenciesRepository;
 import repositories.ExchangeRateRepository;
+import utlis.GetParameters;
 import utlis.JSONResponser;
 import utlis.PathChecker;
 
@@ -24,6 +26,15 @@ public class ExchangeRateServlet extends HttpServlet {
     public ExchangeRateServlet() throws ClassNotFoundException {
         this.currenciesRepository = new CurrenciesRepository(new SQLiteDBManager());
         this.exchangeRateRepository = new ExchangeRateRepository(new SQLiteDBManager(), currenciesRepository);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if ("PATCH".equalsIgnoreCase(req.getMethod())) {
+            doPatch(req, resp);
+        } else {
+            super.service(req, resp);
+        }
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -56,6 +67,42 @@ public class ExchangeRateServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
+    }
+
+    @Override
+    protected void doPatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter pw = response.getWriter();
+        request.setCharacterEncoding("UTF-8");
+
+        String path = request.getPathInfo();
+
+        Map<String, String> params = GetParameters.parseFormBody(request);
+
+        BigDecimal rate = new BigDecimal(params.get("rate"));
+        Map<String, String> exchangePair = PathChecker.checkExchangeRatePath(path);
+        ExchangeRateModel updatedExchangeRateModel;
+
+        if (exchangePair == null) {
+            String message = "Неправильный запрос на обновление обеменного курса. Нужно предоставить пару кодов валют, например /USDRUB";
+            JSONResponser.sendJSONErrorMessage(message, 400, response);
+        } else {
+            try {
+                ExchangeRateModel exchangeRateModel = exchangeRateRepository.findByExchangePair(exchangePair);
+                updatedExchangeRateModel = exchangeRateRepository.update(
+                        new ExchangeRateModel(exchangeRateModel.getID(),
+                                exchangeRateModel.getBaseCurrency(),
+                                exchangeRateModel.getTargetCurrency(),
+                                rate)
+                );
+
+
+                JSONObject jsonObject = new JSONObject(updatedExchangeRateModel);
+                pw.write(jsonObject.toString());
+            } catch (SQLException e) {
+                throw  new RuntimeException(e);
+            }
+        }
     }
 
     public void destroy() {
