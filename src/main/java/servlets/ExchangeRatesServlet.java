df45062aeb1e5b43bb54c8eccd2f042c863b56a9
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-02-11 19:30:56 +0300

    implement POST method for ExchangeRatesServlet

--- src/main/java/servlets/ExchangeRatesServlet.java
+++ src/main/java/servlets/ExchangeRatesServlet.java
@@ -1,15 +1,20 @@
 package servlets;
 
 import java.io.*;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import dbManager.SQLiteDBManager;
+import exceptions.NotFoundInDatabaseException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import models.CurrencyModel;
 import models.ExchangeRateModel;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import repositories.CurrenciesRepository;
 import repositories.ExchangeRateRepository;
 import utlis.JSONResponser;
@@ -41,6 +46,28 @@ public class ExchangeRatesServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json");
+        PrintWriter pw = response.getWriter();
+
+        String baseCurrencyCode, targetCurrencyCode;
+        BigDecimal rate;
+
+        try {
+            baseCurrencyCode = request.getParameter("baseCurrencyCode");
+            targetCurrencyCode = request.getParameter("targetCurrencyCode");
+            rate = new BigDecimal(request.getParameter("rate"));
+
+            CurrencyModel baseCurrency = currenciesRepository.findByCode(baseCurrencyCode);
+            CurrencyModel targetCurrency = currenciesRepository.findByCode(targetCurrencyCode);
+
+            ExchangeRateModel exchangeRate = exchangeRateRepository.insert(new ExchangeRateModel(baseCurrency, targetCurrency, rate));
+
+            JSONObject jsonObject = new JSONObject(exchangeRate);
+            pw.write(jsonObject.toString());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (NotFoundInDatabaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void destroy() {
